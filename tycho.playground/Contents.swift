7a0fa3a5a2cc65784b61c8d1e9eb0524071d3508
kirai <hectorgarcia@gmail.com>
2015-05-20 14:41:12 +0900

    using stringWithContentsOfFile

--- tycho.playground/Contents.swift
+++ tycho.playground/Contents.swift
@@ -6,24 +6,6 @@ var str = "Hello, playground"
 
 var filename = "tyc2.dat.00"
 
-inputReader.open("tyc2.dat.00")
-
-//var inputStream = NSInputStream(fileAtPath: "tyc2.dat.00")
-//let stream: NSInputStream = NSInputStream(data: data)
-
-
-//var inputBuffer = [UInt8](count: 32478734 * 4, repeatedValue: 0)
-
-//let data: NSData = " .".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
-/*let stream: NSInputStream = NSInputStream(fileAtPath: "tyc2.dat.00")!
-var buffer = [UInt8](count: 8, repeatedValue: 0)
-
-stream.open()
-
-if stream.hasBytesAvailable {
-    let result :Int = stream.read(&buffer, maxLength: buffer.count)
-    print(result)
-}*/
 
 //var data :NSData? = FileUtility.dataFromPath("tyc2.dat.00") as? NSData
 let file = "tyc2.dat.00"
@@ -33,7 +15,7 @@ if let dirs : [String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirecto
     let dir = dirs[0] //documents directory
     let path = dir.stringByAppendingPathComponent(file);
     let text = "some text"
-    print text
+    print(text)
 
     //writing
     //text.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding, error: nil);
@@ -43,3 +25,9 @@ if let dirs : [String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirecto
     print(text2)
 }
 
+/*let bundle = NSBundle.mainBundle()
+let path = bundle.pathForResource("tyc2.dat.00", ofType: "txt")
+let content = NSString.stringWithContentsOfFile(path) as! String
+
+println(content) // prints the content of data.txt
+*/
