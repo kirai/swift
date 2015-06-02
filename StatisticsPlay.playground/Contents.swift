5e87def494897c465b06c1bfb447042e336e232e
kirai <hectorgarcia@gmail.com>
2015-06-02 13:56:24 +0900

    a

--- StatisticsPlay.playground/Contents.swift
+++ StatisticsPlay.playground/Contents.swift
@@ -41,15 +41,16 @@ func calcVar(numbers: [Double], avg: Double) -> Double {
 /* Volatility σ: square root of variance                     */
 /*               (StdDev) σ = sqrt(Variance)                 */
 /*************************************************************/
+
 func calcStdDev(variance: Double) -> Double {
     return sqrt(variance)
 }
-
 
 var population: [Double] = [5, 6, 11, 13, 19, 20, 25, 26, 28, 37, 28, 90]
 var average: Double = 0.0
 var variance: Double = 0.0
 var standard_deviation: Double = 0.0
+
 
 average = calcAvg(population)
 println("The average population age is \(average)")
@@ -59,6 +60,8 @@ println("The variance of the population age is \(variance)")
 
 standard_deviation = calcStdDev(variance)
 println("The variance of the population age is \(standard_deviation)")
+
+// Try to put fat tails and chart, test fragility
 
 // Getting data from google finance API
 // let url = NSURL(string: "http://www.kirainet.com")
